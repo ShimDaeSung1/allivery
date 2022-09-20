2f3938da5af047681bcb523adf1daa09be18f4be
Hyun Woo <hwsa10041@gmail.com>
2022-09-20 14:23:06 +0900

    구글 로그인 Controller, Service 구현
    필요한 Service 주석으로 표시해놨습니다.

--- src/main/java/org/zerock/allivery/controller/OAuthController.java
+++ src/main/java/org/zerock/allivery/controller/OAuthController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.zerock.allivery.config.GoogleOAuth;
 import org.zerock.allivery.service.OAuthService;
 
 import javax.servlet.http.HttpServletResponse;
@@ -19,6 +20,8 @@ import java.io.IOException;
 public class OAuthController {
 
     private final OAuthService oAuthService;
+    private final GoogleOAuth googleOAuth;
+
 
     @GetMapping("/kakao")
     public void getKakakoAuthUrl(HttpServletResponse response) throws IOException {
@@ -30,5 +33,17 @@ public class OAuthController {
             @RequestParam(name = "code") String code) throws IOException {
         log.info("카카오 API 서버 code : " + code);
         return oAuthService.kakaoLogin(code);
+    }
+
+    @GetMapping("/google")
+    public void getGoogleAuthUrl(HttpServletResponse response) throws IOException {
+        response.sendRedirect(googleOAuth.getOauthRedirectURL());
+    }
+
+    @GetMapping("/google/login")
+    public ResponseEntity<String> callback(
+            @RequestParam(name = "code") String code) throws IOException {
+        log.info("구글 API 서버 code : " + code);
+        return oAuthService.googlelogin(code);
     }
 }
--- src/main/java/org/zerock/allivery/service/OAuthService.java
+++ src/main/java/org/zerock/allivery/service/OAuthService.java
@@ -5,7 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.zerock.allivery.config.GoogleOAuth;
 import org.zerock.allivery.config.KakaoOAuth;
+import org.zerock.allivery.dto.OAuth.GoogleOAuthTokenDto;
+import org.zerock.allivery.dto.OAuth.GoogleUserInfoDto;
 import org.zerock.allivery.dto.OAuth.KakaoOAuthTokenDto;
 import org.zerock.allivery.dto.OAuth.KakaoUserInfoDto;
 import org.zerock.allivery.entity.user.User;
@@ -20,6 +23,7 @@ import java.time.LocalDateTime;
 public class OAuthService {
     private final UserRepository userRepository;
     private final KakaoOAuth kakaoOAuth;
+    private final GoogleOAuth googleOAuth;
 
     // Service
     // 카카오 로그인 서비스
@@ -52,4 +56,43 @@ public class OAuthService {
          */
         return new ResponseEntity<>("LOGIN_TRUE", HttpStatus.OK);
     }
+
+    public ResponseEntity<String> googlelogin(String code) throws IOException {
+        // 구글로 일회성 코드를 보내 액세스 토큰이 담긴 응답객체를 받아옴
+        ResponseEntity<String> accessTokenResponse = googleOAuth.requestAccessToken(code);
+        // 응답 객체가 JSON 형식으로 되어 있으므로, 이를 deserialization해서 자바 객체에 담을 것이다.
+        GoogleOAuthTokenDto oAuthToken = googleOAuth.getAccessToken(accessTokenResponse);
+        // accessToken을 담은 후 accessToken 통신
+        ResponseEntity<String> userInfoResponse = googleOAuth.requestUserInfo(oAuthToken);
+        GoogleUserInfoDto googleUser = googleOAuth.getUserInfo(userInfoResponse);
+
+        String email = googleUser.getEmail();
+        String name = googleUser.getName();
+
+        if (!userRepository.existsByEmail(email)) {
+            /*
+             * 회원가입일때와 로그인일때를 구분해서 JWT 발급이 가능해야함
+             */
+            userRepository.save(
+                    User.builder()
+                            .email(email)
+                            .nickName(name)
+                            .password("google")
+                            .serialNum(name + LocalDateTime.now())
+                            .build()
+            );
+            return new ResponseEntity<>("REGISTER_TRUE", HttpStatus.OK);
+            /*
+             * body에 클라이언트로 보낼 JSON, 현재 임의로 String
+             */
+        }
+        /*
+         * body에 클라이언트로 보낼 JSON, 현재 임의로 String
+         */
+        return new ResponseEntity<>("LOGIN_TRUE",HttpStatus.OK);
+    }
+    /*
+    회원가입 이후 추가정보 요청하는 Service 필요
+    성공시 회원가입 또는 로그인 시의 Validation 필요
+     */
 }
